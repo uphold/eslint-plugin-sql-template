3909d506af3d2e8422a6924f239553b8846fe70a
Ricardo Lopes <ricardo@uphold.com>
2016-09-30 11:02:30 +0100

    Create project with `no-unsafe-query` rule

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * Export rules.
+ */
+
+module.exports.rules = {
+  'no-unsafe-query': require('./rules/no-unsafe-query')
+};
---
+++ rules/no-unsafe-query.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+const _ = require('lodash');
+const parser = require('sql-parse');
+
+/**
+ * Check if `literal` is an SQL query.
+ */
+
+function isSqlQuery(literal) {
+  if (!literal) {
+    return false;
+  }
+
+  try {
+    parser.parse(literal);
+  } catch (error) {
+    return false;
+  }
+
+  return true;
+}
+
+/**
+ * Validate node.
+ */
+
+function validate(node, context) {
+  if (!node) {
+    return;
+  }
+
+  if (node.type === 'TaggedTemplateExpression' && node.tag.name !== 'sql') {
+    node = node.quasi;
+  }
+
+  if (node.type === 'TemplateLiteral' && node.expressions.length) {
+    const literal = _.map(node.quasis, 'value.raw').join('x');
+
+    if (isSqlQuery(literal)) {
+      context.report(node, 'Use the `sql` tagged template literal for raw queries');
+    }
+  }
+}
+
+/**
+ * Export `no-unsafe-query`.
+ */
+
+module.exports = context => ({
+  CallExpression(node) {
+    node.arguments.forEach(argument => validate(argument, context));
+  },
+  VariableDeclaration(node) {
+    node.declarations.forEach(declaration => validate(declaration.init, context));
+  }
+});
